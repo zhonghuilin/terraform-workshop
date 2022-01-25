13a74d09cb6ef9678e04f29a91cfbd754cbfe8c7
zhonghuilin <zhonghuilin69@gmail.com>
2022-01-25 11:40:03 -0500

    Update main.tf

--- 0000-setup/main.tf
+++ 0000-setup/main.tf
@@ -9,11 +9,13 @@ terraform {
 }
 
 provider "aws" {
+  access_key = "AKIARLSHEJ4LMHIY5N7F"
+  secret_key = "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"
   region = "us-east-1"
 }
 
 resource "aws_instance" "exercise_0000" {
-  ami           = "ami-07ebfd5b3428b6f4d"
+  ami           = "ami-066157edddaec5e49"
   instance_type = "t2.micro"
 
   tags = {
